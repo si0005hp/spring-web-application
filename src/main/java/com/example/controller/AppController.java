51de2c3dd554b7f40f4813589488a58a1bee7e45
si0005hp <si0005hp@gmail.com>
2016-07-05 05:13:56 +0900

    Add smoke test resources.

--- src/main/java/com/example/controller/AppController.java
+++ src/main/java/com/example/controller/AppController.java
@@ -1,13 +1,20 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.IService;
 
 @RestController
 @RequestMapping("/app")
 public class AppController {
 
+//	@Autowired(required = false)
+	@Autowired
+	private IService service;
+	
 	@ResponseBody
 	@RequestMapping("/")
 	public String root() {
